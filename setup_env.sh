c1e9709e3cc73c52aefd8afd40c23c6463a7ecf9
Arsanandha Abhisidvorachodi <arsanandha.ap@gmail.com>
2023-05-27 16:21:47 +0700

    nginx fix

--- setup_env.sh
+++ setup_env.sh
@@ -1,5 +1,5 @@
-touch /certs/origin-cert.pem
-touch /certs/private-key-cert.key
+touch certs/origin-cert.pem
+touch certs/private-key-cert.key
 
-echo "${SSL_ORIGIN_CERT}" >> /certs/origin-cert.pem
-echo "${SSL_PRIVATE_KEY}" >> /certs/private-key-cert.key
+echo "$SSL_ORIGIN_CERT" >> certs/origin-cert.pem
+echo "$SSL_PRIVATE_KEY" >> certs/private-key-cert.key
