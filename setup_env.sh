f74725f7383cce0a3c451a6a6fc06c8702215140
Arsanandha Abhisidvorachodi <arsanandha.ap@gmail.com>
2023-05-28 14:10:56 +0700

    nginx fix

--- setup_env.sh
+++ setup_env.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 touch certs/origin-cert.pem
 touch certs/private-key-cert.key
+touch certs/origin-client-cert.pem
+touch certs/dhparam.pem
+touch certs/ca-bundle.pem
 
 echo "$SSL_ORIGIN_CERT" >> certs/origin-cert.pem
 echo "$SSL_PRIVATE_KEY" >> certs/private-key-cert.key
-
-cat certs/origin-cert.pem
+echo "$SSL_CLIENT_CERT" >> certs/origin-client-cert.pem
+echo "$SSL_DHPARAM" >> certs/dhparam.pem
+echo "$SSL_CA_BUNDLE" >> certs/ca-bundle.pem
