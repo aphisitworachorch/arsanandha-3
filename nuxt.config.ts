80b651036eafd404125d18d89d6e5d74f05d51e9
Arsanandha Abhisidvorachodi <arsanandha.ap@gmail.com>
2023-05-29 14:27:47 +0700

    add icon for ap

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,7 +5,32 @@ export default defineNuxtConfig({
             charset: 'UTF-8',
             htmlAttrs: {
                 lang: 'en'
-            }
+            },
+            link: [
+                { rel: 'apple-touch-icon-precomposed', sizes: '57x57', href: 'https://arsanandha.xyz/public/icon/apple-touch-icon-57x57.png' },
+                { rel: 'apple-touch-icon-precomposed', sizes: '114x114', href: 'https://arsanandha.xyz/public/icon/apple-touch-icon-114x114.png' },
+                { rel: 'apple-touch-icon-precomposed', sizes: '72x72', href: 'https://arsanandha.xyz/public/icon/apple-touch-icon-72x72.png' },
+                { rel: 'apple-touch-icon-precomposed', sizes: '144x144', href: 'https://arsanandha.xyz/public/icon/apple-touch-icon-144x144.png' },
+                { rel: 'apple-touch-icon-precomposed', sizes: '60x60', href: 'https://arsanandha.xyz/public/icon/apple-touch-icon-60x60.png' },
+                { rel: 'apple-touch-icon-precomposed', sizes: '120x120', href: 'https://arsanandha.xyz/public/icon/apple-touch-icon-120x120.png' },
+                { rel: 'apple-touch-icon-precomposed', sizes: '76x76', href: 'https://arsanandha.xyz/public/icon/apple-touch-icon-76x76.png' },
+                { rel: 'apple-touch-icon-precomposed', sizes: '152x152', href: 'https://arsanandha.xyz/public/icon/apple-touch-icon-152x152.png' },
+                { rel: 'icon', type: 'image/png', href: 'https://arsanandha.xyz/public/icon/favicon-196x196.png', sizes: '196x196' },
+                { rel: 'icon', type: 'image/png', href: 'https://arsanandha.xyz/public/icon/favicon-96x96.png', sizes: '96x96' },
+                { rel: 'icon', type: 'image/png', href: 'https://arsanandha.xyz/public/icon/favicon-32x32.png', sizes: '32x32' },
+                { rel: 'icon', type: 'image/png', href: 'https://arsanandha.xyz/public/icon/favicon-16x16.png', sizes: '16x16' },
+                { rel: 'icon', type: 'image/png', href: 'https://arsanandha.xyz/public/icon/favicon-128.png', sizes: '128x128' }
+            ],
+            meta: [
+                { name: 'application-name', content: 'ARSANANDHA.XYZ' },
+                { name: 'theme-color', content: '#043295' },
+                { name: 'msapplication-TileColor', content: '#043295 },
+                { name: 'msapplication-TileImage', content: 'https://arsanandha.xyz/public/icon/mstile-144x144.png' },
+                { name: 'msapplication-square70x70logo', content: 'https://arsanandha.xyz/public/icon/mstile-70x70.png' },
+                { name: 'msapplication-square150x150logo', content: 'https://arsanandha.xyz/public/icon/mstile-150x150.png' },
+                { name: 'msapplication-wide310x150logo', content: 'https://arsanandha.xyz/public/icon/mstile-310x150.png' },
+                { name: 'msapplication-square310x310logo', content: 'https://arsanandha.xyz/public/icon/mstile-310x310.png' }
+            ]
         },
     },
     css: ['animate.css/animate.min.css'],
