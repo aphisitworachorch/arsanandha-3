5f6ceb39564e636016fa2ed4d55b3a2543d02b71
Arsanandha Abhisidvorachodi <arsanandha.ap@gmail.com>
2023-06-26 13:49:38 +0700

    Upgrade Nuxt

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -43,7 +43,7 @@ export default defineNuxtConfig({
         download: true,
         base64: true,
         display: 'auto',
-    }]],
+    }],'nuxt-security'],
     nitro: {
         compressPublicAssets: true,
     },
