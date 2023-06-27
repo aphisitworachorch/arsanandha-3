e9b4bbc0d9868a9b532238a0d1aea1d0708e2b10
Arsanandha Abhisidvorachodi <arsanandha.ap@gmail.com>
2023-06-27 18:59:30 +0700

    add: Add authentication and login support for Spotify
    This commit adds authentication support using the '@sidebase/nuxt-auth' library, with configuration updates in nuxt.config.ts.
    - Added @nuxtjs/auth-next, @nuxtjs/axios, and @sidebase/nuxt-auth to package.json
    - Implemented the authentication state in TotoProfileComponents.vue
    - getUser, signIn, and signOut functions implemented for handling user authentication
    
    Integrates Spotify login with the 'next-auth' library:
    - New server/api/auth/[...].ts file to handle authentication
    - Added 'next-auth' dependencies in package.json
    - Updated setup_env.sh with new environment variables
    - Updated pnpm-lock.yaml file

--- components/TotoProfileComponents.vue
+++ components/TotoProfileComponents.vue
@@ -1,10 +1,39 @@
+<script setup>
+const {
+  status,
+  data,
+  lastRefreshedAt,
+  getCsrfToken,
+  getProviders,
+  getSession,
+  signIn,
+  signOut,
+} = useAuth()
+</script>
 <template>
   <div class="container font-apFont">
+    <div class="transition-all duration-50 p-3 text-left" v-if="status === 'authenticated'">
+      <div class="card md:w-96 w-60 bg-primary shadow-xl">
+        <div class="flex flex-row m-3 items-center">
+          <div class="pr-5">
+            <div class="avatar items-center pt-2">
+              <div class="w-12 rounded-full">
+                <img :src="data.user.image"  alt="Spotify Badges"/>
+              </div>
+            </div>
+          </div>
+          <div>
+            <h2 class="font-apMonoFont font-bold">Hi, {{ data.user.name }}!</h2>
+            <h2 class="font-apMonoFont font-light">You're Logged In!</h2>
+          </div>
+        </div>
+      </div>
+    </div>
     <div class="flex flex-col md:flex-row m-3">
       <div class="transition-all duration-50 p-2">
         <div class="transition-all duration-50 avatar">
           <div class="transition-all duration-50 max-w-screen md:w-96 rounded-b-xl rounded-t-xl animate__animated animate__fadeIn">
-            <nuxt-img format="webp" src="/pics/toto-alternative.jpg" width="1200" height="1200" sizes="sm:70vw md:60vw lg:900px" alt="Profile with 1 Person" />
+            <nuxt-img fit="cover" format="webp" src="/pics/toto-alternative.jpg" width="1200" sizes="sm:70vw md:60vw lg:900px" alt="Profile with 1 Person" />
           </div>
         </div>
       </div>
@@ -21,12 +50,15 @@
         </div>
         <div class="dropdown p-3">
           <label tabindex="0" class="transition-all duration-50 btn m-1 text-white">View Profile</label>
-          <ul tabindex="0" class="dropdown-content menu p-2 shadow bg-neutral text-white rounded-box w-52">
-            <li v-for="data in menu" :key="data.name">
-              <nuxt-link :to="data.route">{{ data.name }} <div class="badge badge-primary" v-if="data.isnew">NEW 🎉</div></nuxt-link>
+          <ul tabindex="0" class="dropdown-content menu p-2 shadow bg-neutral text-white rounded-box w-60">
+            <li v-for="mn in menu" :key="mn.name">
+              <nuxt-link :to="mn.route">{{ mn.name }} <div class="badge badge-primary" v-if="mn.isnew">NEW 🎉</div></nuxt-link>
             </li>
             <li>
               <a :href="resume">Resume<div class="badge badge-primary">NEW 🎉</div></a>
+            </li>
+            <li>
+              <button @click="signIn('spotify')">Login to Spotify<div class="badge badge-primary">EXP 💥</div></button>
             </li>
           </ul>
         </div>
@@ -68,7 +100,7 @@ export default {
           }
         ];
       }, 300);
-    }
+    },
   },
   created() {
     this.showName();
--- setup_env.sh
+++ setup_env.sh
@@ -1,1 +1,11 @@
 #!/bin/bash
+
+touch .env
+
+echo "AP_API_ENDPOINT=$AP_API_ENDPOINT" >> .env
+echo "AUTH_ORIGIN=$AUTH_ORIGIN" >> .env
+echo "NEXTAUTH_URL=$NEXTAUTH_URL" >> .env
+echo "SECRET=$SECRET" >> .env
+echo "SPOTIFY_CLIENT_ID=$SPOTIFY_CLIENT_ID" >> .env
+echo "SPOTIFY_CLIENT_SECRET=$SPOTIFY_CLIENT_SECRET" >> .env
+echo "SPOTIFY_CALLBACK_URL=$SPOTIFY_CALLBACK_URL" >> .env
